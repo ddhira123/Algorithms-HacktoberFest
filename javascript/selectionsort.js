6e1ee7b08b066696bbad4de7ac9948a29e66702d
Tilak <1ms17s124@gmail.com>
2019-10-03 16:13:51 +0530

    Selection sort JS
    Signed-off-by: Tilak <1ms17s124@gmail.com>

---
+++ javascript/selectionsort.js
@@ -0,0 +1,26 @@
+// array to sort
+var array = [90, 21, 52, 60, 45, 30, 17, 10, 15, 18];
+
+// swap function helper
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function selectionSort(array) {
+  for(var i = 0; i < array.length; i++) {
+    var min = i;
+    for(var j = i + 1; j < array.length; j++) {
+      if(array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if(i !== min) {
+      swap(array, i, min);
+    }
+  }
+  return array;
+}
+
+console.log(selectionSort(array)); 
