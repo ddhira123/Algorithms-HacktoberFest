918efd8b8c7be510e12cae92f68bb3a8fc9eceaa
Shreshth Malik <shreshthmalik99@gmail.com>
2019-10-12 18:36:43 +0530

    Optimised Bubble sort in java

--- Java/Bubble_Sort.java
+++ Java/Bubble_Sort.java
@@ -1,32 +1,34 @@
-public class BubbleSort {
+public class Bubble_Sort {
 
 	public static void main(String[] args) {
-		
+
 		int arr[] = {2,7,5,1,8,3};
-		
-		for(int i=0;i<arr.length;i++){
-			for(int j=i+1;j<arr.length;j++){
-				if(arr[i]>arr[j]){
-					swap(arr,i,j);
+		boolean swapped;
+		int temp;
+
+		for(int i=0;i<arr.length-1;i++){
+
+			swapped=false;
+
+			for(int j=0;j<arr.length-i-1;j++){
+
+				if(arr[j]>arr[j+1])
+				{
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+					swapped =true;
 				}
+
 			}
+			if(swapped==false)
+			break;
 		}
-		
+
 		for(int i =0;i<arr.length;i++){
 			System.out.println(arr[i]);
 		}
 
-	}
-
-	public static void swap(int arr[],int i,int j){
-		
-		if(i==j){
-			return;
-		}
-		int temp = arr[i];
-		arr[i]=arr[j];
-		arr[j]=temp;
-		
 	}
 
 }
